f3577d9b140d0afe52240652a21e0e2491eb6639
Ashenafidejene <ashenafidejene75@gmail.com>
2023-11-19 21:28:45 +0300

    route is updated

--- app/api/project/route.ts
+++ app/api/project/route.ts
@@ -1,13 +1,18 @@
 import { NextRequest } from "next/server"
 import {z} from 'zod';
-const createProjectSchema=z.object({
-    projectName:z.string().min(1,"the minimum character is need only one character").max(255,'it is the longer name you must decrease the size'),
-    description : z.string().min(1),
-
-})
-const createTeamSchema=z.object({
-    
-})
+const createProjectSchema = z.object({
+    projectName: z.string().min(1, "The project name must have at least 1 character"),
+    description: z.string().min(1, "The project description must have at least 1 character"),
+  });
+  
+  const createTeamMemberSchema = z.object({
+    email: z.string().email("Invalid email address").nonempty("Email is required"),
+  });
+  
+  const createStakeholderSchema = z.object({
+    email: z.string().email("Invalid email address").nonempty("Email is required"),
+  });
 export async function POST(request:NextRequest){
     const body = await request.json
+    
 }
