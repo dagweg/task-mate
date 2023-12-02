beb5faf23e2da4f54549d3377e7efc74276c0df9
Amanuel-1 <amanuelgaromsa@gmail.com>
2023-12-02 17:04:26 +0300

    fix:beautified the projects page

--- app/(site)/projects/page.tsx
+++ app/(site)/projects/page.tsx
@@ -2,8 +2,9 @@
 
 import Link from 'next/link'
 import React, { ReactElement, useEffect, useState } from 'react'
-import supabase from '../config/supabaseClient'
-import constants from '../data/constants'
+import supabase from '../../config/supabaseClient'
+import constants from '../../data/constants'
+import { shortener } from '@/app/lib/utils'
 
 
 interface Project {
@@ -33,15 +34,15 @@ function Projects() {
     }, [])
 
     return (
-        <div className='grid grid-cols-3  grid-rows-2 justify-center h-screen gap-8'>
+        <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 justify-center h-screen gap-8'>
 
             {
                 projects.map((project: any, index: number) => (
                     <Link href={'/project/[id]'} as={`/project/${project.name}`} key={index}>
-                        <div className='relative  overflow-hidden bg-[#a6a1b1] w-full h-fit max-w-[500px] p-20 flex flex-col rounded-3xl shadow-sm bg-opacity-10 hover:scale-105 duration-200 hover:bg-white hover:shadow-2xl cursor-pointer active:scale-100'>
-                            <h1 className='font-bold tracking-wide text-2xl py-2'>{project.name}</h1>
-                            <p className='text-sm'>
-                                {project.description}
+                        <div className='flex flex-col justify-around cols-span-1  overflow-hidden bg-[#a6a1b1] w-full h-full max-w-[500px] p-7 rounded-3xl shadow-sm bg-opacity-10 hover:scale-[101%] duration-200 hover:bg-white hover:shadow-2xl cursor-pointer active:scale-100'>
+                            <h1 className='font-bold tracking-wide text-2xl py-2'>{shortener(project.name,22)}</h1>
+                            <p className='text-sm text-justify '>
+                                {shortener(project.description,200)}
                             </p>
                             <div className='bg-gray-200 w-fit p-2 rounded-lg font-semibold shadow-sm mt-8'>
                                 {project.members_count}/25 Members
--- app/lib/utils.ts
+++ app/lib/utils.ts
@@ -5,4 +5,17 @@ import { twMerge } from "tailwind-merge";
 export function cn(...inputs: ClassValue[]) {
     return twMerge(clsx(inputs))
   }
+
+
+  export function shortener(text: string,size:number): string {
+    let result: string; // Declare the type of 'result' as string
+  
+    if (text.length > size) {
+      result = text.substring(0, size) + "...";
+    } else {
+      result = text;
+    }
+  
+    return result;
+  }
   
