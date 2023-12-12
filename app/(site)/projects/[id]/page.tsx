5bbc85bb36cb87af5ab9a6f4e02fc0abc1083118
dagweg <dagtef2@gmail.com>
2023-12-12 09:20:02 +0300

    upd

--- app/(site)/projects/[id]/page.tsx
+++ app/(site)/projects/[id]/page.tsx
@@ -5,12 +5,10 @@ import { usePathname } from 'next/navigation'
 
 function Project() {
 
-    const pathname = usePathname();
+    const pathname = usePathname()
 
     let paths = pathname.split('/')
     let path = decodeURIComponent(paths[paths.length - 1])
-
-
 
     return (
         <div className='text-2xl'>Project: <span className='font-bold'>{path}</span></div>
--- app/(site)/projects/layout.tsx
+++ app/(site)/projects/layout.tsx
@@ -7,7 +7,7 @@ function ProjectsLayout({ children }: { children: ReactNode }) {
         <div className='p-20 flex flex-col gap-10 h-fit '>
             <div className='flex justify-between w-full items-center'>
                 <h1 className='text-5xl font-bold '>Projects</h1>
-                <AddProjectDropdownButton/>
+                {/* <AddProjectDropdownButton/> */}
                 <Link href={'/projects/add'}><div className='bg-dark2 text-white p-4 rounded-lg hover:bg-dark1 cursor-pointer active:scale-105 duration-150 ease-in-out'><i className='fa-solid fa-plus mx-4'></i>Add Project</div></Link>
             </div>
             {children}
