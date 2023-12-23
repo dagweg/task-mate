e6a1a7328e33dafeeb08f73f4b4e090d900b3c56
DagW <dagtef@gmail.com>
2023-12-23 08:01:02 +0300

    u

--- app/(site)/projects/my-projects/page.tsx
+++ app/(site)/projects/my-projects/page.tsx
@@ -50,7 +50,7 @@ function MyProjects() {
                     :
                     [...projects].map((project: any, index: number) => (
                         <Link href={'/project/[id]'} as={`/project/project?pname=${project.title}&pid=${project.id}`} key={index}>
-                            <div className='flex flex-col justify-around cols-span-1  overflow-hidden bg-[#a6a1b1] w-full h-fit max-w-[500px] p-7 rounded-[10px] shadow-sm bg-opacity-10 hover:scale-[101%] duration-200 hover:bg-white border-2 hover:border-red-200 hover:shadow-2xl cursor-pointer active:scale-100'>
+                            <div className='flex flex-col justify-around cols-span-1  overflow-hidden bg-[#a6a1b1] w-full h-fit max-w-[500px] p-7 rounded-[10px] shadow-sm bg-opacity-10 hover:scale-[101%] duration-200 hover:bg-white border-2 hover:border-slate-500 hover:shadow-2xl cursor-pointer active:scale-100'>
                                 <h1 className='font-bold tracking-wide text-2xl py-2'>{project.title}</h1>
                                 <p className='text-sm text-justify '>
                                     {project.description}
