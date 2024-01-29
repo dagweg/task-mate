e3537e93f72b70fbf73646088a26caac2e0520ab
DAG WEG <dagtef2@gmail.com>
2024-01-29 23:49:58 +0300

    next config

--- next.config.js
+++ next.config.js
@@ -8,7 +8,12 @@ const nextConfig = {
                 hostname: "**",
             },
         ],
-    }
+    },
+    eslint: {
+        // Warning: This allows production builds to successfully complete even if
+        // your project has ESLint errors.
+        ignoreDuringBuilds: true,
+    },
 }
 
 module.exports = nextConfig
