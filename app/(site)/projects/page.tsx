3c6ffa644b7b3882b11de69945e93814e193a28c
DAG WEG <dagtef2@gmail.com>
2024-01-29 18:57:40 +0300

    more refactoring

--- app/(site)/projects/page.tsx
+++ app/(site)/projects/page.tsx
@@ -1,23 +1,10 @@
 'use client'
 
 import Link from 'next/link'
-import React, { ReactElement, ReactNode, useEffect, useState } from 'react'
-import supabase from '../../config/supabaseClient'
-import constants from '../../data/constants'
-import { cn } from '@/app/lib/utils'
+import React from 'react'
 import MyProjects from './my-projects/page'
-// import { shortener } from '@/app/lib/utils'
 
-
-interface Project {
-    name: string,
-    description: string,
-    members_count: number
-}
-
-function Projects({ children }: { children: ReactNode }) {
-
-
+function Projects() {
     return (
         <>
             <MyProjects />
